b68422c45c2d729e1be5cb13945b34a3cb69fcb3
dinhnq-gihot <dinh.nguyen@gihot.vn>
2024-12-23 15:27:50 +0700

    chore(db): db migrations

---
+++ migrations/2024-12-21-033112_create_messages/up.sql
@@ -0,0 +1,11 @@
+-- Your SQL goes here
+CREATE TABLE messages (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES users(id),
+    group_id UUID NOT NULL REFERENCES groups(id),
+    content TEXT,
+    type message_types DEFAULT 'text',
+    name_file VARCHAR,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    edited_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
